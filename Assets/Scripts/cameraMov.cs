d5b856107673b49710bc55fd349ce8e9424fc8c6
daoyee <acgfrog@outlook.com>
2018-12-13 22:40:51 -0800

    fix unity version, add mouse

---
+++ Assets/Scripts/cameraMov.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMov : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float yaw = 0;  
+    float pitch =0;
+    float speedH =3;
+    float speedV=3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += speedH*Input.GetAxis("Mouse X");
+        pitch -= speedV*Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+    }
+}
